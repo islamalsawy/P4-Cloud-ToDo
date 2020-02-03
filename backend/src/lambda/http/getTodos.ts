2a0cbe063026755b12c840a17d9a4f2e6156e97d
Islam <islam.elafefe@sed.tanta.edu.eg>
2020-02-03 22:46:14 +0200

    first commit

---
+++ backend/src/lambda/http/getTodos.ts
@@ -0,0 +1,21 @@
+import 'source-map-support/register';
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda';
+
+// dev imported
+import { getTodos } from '../../businessLogic/todos';
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: await getTodos(event)
+    })
+  };
+}
